e60ee9cfd2c9024ef0f5dbde773c73754e981b5b
alijamal83 <AliAlwasseti83@gmail.com>
2022-03-30 03:37:12 +0200

    modify state.html adding new features

--- src/main/java/intec/be/Falconapp/controllers/StateController.java
+++ src/main/java/intec/be/Falconapp/controllers/StateController.java
@@ -1,15 +1,58 @@
 package intec.be.Falconapp.controllers;
 
 
+import com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext;
+import intec.be.Falconapp.models.State;
+import intec.be.Falconapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StateController {
 
-    @GetMapping("/state")
-    public String getState() {
-        return "State";
 
+    @Autowired
+    private StateService stateService;
+
+
+
+    @GetMapping("/states")
+    public String getState(Model model) {
+        List<State> stateList = stateService.getStates();
+        model.addAttribute("states", stateList);
+        return "State";
+    }
+
+
+    @PostMapping("/state/addNewState")
+    public String addNewState(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+
+    @RequestMapping("states/findById")
+    @ResponseBody
+    public Optional<State> findById(Integer id) {
+        return stateService.findById(id);
+    }
+
+
+    @RequestMapping(value = "/states/update", method= {RequestMethod.PUT, RequestMethod.GET})
+    public String Update(State state) {
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+
+    @RequestMapping(value = "/states/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        stateService.delete(id);
+        return "redirect:/states";
     }
 }
--- src/main/java/intec/be/Falconapp/services/StateService.java
+++ src/main/java/intec/be/Falconapp/services/StateService.java
@@ -1,8 +1,42 @@
 package intec.be.Falconapp.services;
 
 
+import intec.be.Falconapp.models.State;
+import intec.be.Falconapp.repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StateService {
+
+
+    //Inject the service into the repository
+    @Autowired
+    private StateRepository stateRepository;
+
+
+    //Return list of states
+    public List<State> getStates() {
+        return stateRepository.findAll();
+
+    }
+
+    //Save new state
+    public void save(State state) {
+        stateRepository.save(state);
+    }
+
+    //Get by id
+    public Optional<State> findById(Integer id) {
+        return stateRepository.findById(id);
+    }
+
+    // Delete state from state list
+
+    public void delete(Integer id) {
+        stateRepository.deleteById(id);
+    }
 }
