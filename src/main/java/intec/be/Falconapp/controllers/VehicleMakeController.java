594a69ace33d88dcf07695c8ceead8a8075403dd
alijamal83 <AliAlwasseti83@gmail.com>
2022-05-01 05:05:00 +0200

    adjust the sidebare for the template in the html file

--- src/main/java/intec/be/Falconapp/controllers/VehicleMakeController.java
+++ src/main/java/intec/be/Falconapp/controllers/VehicleMakeController.java
@@ -1,15 +1,59 @@
 package intec.be.Falconapp.controllers;
 
 
+import intec.be.Falconapp.models.VehicleMake;
+import intec.be.Falconapp.services.VehicleMakeService;
+import intec.be.Falconapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class VehicleMakeController {
 
-    @GetMapping("/vehicleMake")
-    public String getVehicleMake() {
+
+    @Autowired
+    private VehicleMakeService vehicleMakeService;
+
+    //Get All Vehicle Makes
+    @GetMapping("/vehicleMakes")
+    public String findAll(Model model) {
+        model.addAttribute("vehicleMakes", vehicleMakeService.findAll());
         return "VehicleMake";
 
     }
+
+
+    @RequestMapping("/vehicleMakes/findById")
+    @ResponseBody
+    public Optional<VehicleMake> findById(Integer id) {
+        return vehicleMakeService.findById(id);
+    }
+
+
+    //Add New VehicleMake
+    @PostMapping (value = "vehicleMakes/addNew")
+    @ResponseBody
+    public String addNew(VehicleMake vehicleMake) {
+        vehicleMakeService.save(vehicleMake);
+        return "redirect:/vehicleMakes";
+    }
+
+
+    @RequestMapping(value ="/vehicleMakes/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    private String update(VehicleMake vehicleMake) {
+        vehicleMakeService.save(vehicleMake);
+        return "redirect:/vehicleMakes";
+    }
+
+
+    @RequestMapping(value = "/vehicleMakes/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        vehicleMakeService.delete(id);
+        return "redirect:/vehicleMakes";
+    }
+
 }
--- src/main/java/intec/be/Falconapp/services/VehicleMakeService.java
+++ src/main/java/intec/be/Falconapp/services/VehicleMakeService.java
@@ -1,8 +1,37 @@
 package intec.be.Falconapp.services;
 
 
+import intec.be.Falconapp.models.VehicleMake;
+import intec.be.Falconapp.repositories.VehicleMakeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMakeService {
+
+    @Autowired
+    private VehicleMakeRepository vehicleMakeRepository;
+
+    //Get All Vehicle make
+    public List<VehicleMake> findAll() {
+        return vehicleMakeRepository.findAll();
+    }
+
+    //Get All Vehicle make By id
+    public Optional<VehicleMake> findById(Integer id){
+        return vehicleMakeRepository.findById(id);
+    }
+
+    //Delete VehicleMake
+    public void delete(Integer id){
+        vehicleMakeRepository.deleteById(id);
+    }
+
+    //Update VehicleMake
+    public void save(VehicleMake vehicleMake){
+        vehicleMakeRepository.save(vehicleMake);
+    }
 }
